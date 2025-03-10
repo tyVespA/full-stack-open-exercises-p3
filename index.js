1e841d65611de3e645a3ebc27a505711ccff367d
tyVespA <migliavacca.marco08@gmail.com>
2025-03-10 15:44:18 +0100

    complete 3.5

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-const data = [
+let data = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -30,6 +31,30 @@ app.get("/api/persons", (req, res) => {
 
 app.get("/info", (req, res) => {
   res.send(`Phonebook has info for ${data.length} people <br> ${new Date()}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const reqId = req.params.id;
+  const person = data.find((person) => person.id === reqId);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+  console.log(person);
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000 + 1).toString(),
+    name: person.name,
+    number: person.number,
+  };
+
+  data = data.concat(newPerson);
+  res.json(person);
 });
 
 const PORT = process.env.PORT || 3001;
