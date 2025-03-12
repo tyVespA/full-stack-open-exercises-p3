ff869e2235e979bb2832a9586c74cc01e849396e
tyVespA <migliavacca.marco08@gmail.com>
2025-03-12 13:30:11 +0100

    complete 3.13, 3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 var morgan = require("morgan");
+const Person = require("./models/person");
 
 morgan.token("reqBody", function (req, res) {
   if (req.method === "POST") {
@@ -19,45 +21,24 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
-let data = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/people", (req, res) => {
-  res.json(data);
+  Person.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
 app.get("/info", (req, res) => {
-  res.send(`Phonebook has info for ${data.length} people <br> ${new Date()}`);
+  Person.find({}).then((people) => {
+    res.send(
+      `Phonebook has info for ${people.length} people <br> ${new Date()}`
+    );
+  });
 });
 
 app.get("/api/people/:id", (req, res) => {
-  const reqId = req.params.id;
-  const person = data.find((person) => person.id === reqId);
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.post("/api/people", (req, res) => {
@@ -71,20 +52,22 @@ app.post("/api/people", (req, res) => {
     return res.status(400).json({
       error: "number missing",
     });
-  } else if (data.find((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
   }
+  // else if (data.find((person) => person.name === body.name)) {
+  //   return res.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
-    id: Math.floor(Math.random() * 10000 + 1).toString(),
+  const person = new Person({
+    // id: Math.floor(Math.random() * 10000 + 1).toString(),
     name: body.name,
     number: body.number,
-  };
+  });
 
-  data = data.concat(person);
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
